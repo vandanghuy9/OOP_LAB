a4df258f332c9b65ea70e8e59945c2032404691b
vandanghuy9 <vandanghuy9@gmail.com>
2022-11-10 23:21:31 +0700

    first

---
+++ DigitalVideoDisc.java
@@ -0,0 +1,59 @@
+package OOP_WEEK_2;
+
+public class DigitalVideoDisc {
+	private String title;
+	private String category;
+	private String director;
+	private int length;
+	private float cost;
+	
+	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		this.title = title;
+		this.category= category;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+	}
+	
+	public DigitalVideoDisc(String title, String category, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+	}
+
+	public DigitalVideoDisc(String title, String category, String director, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.cost = cost;
+	}
+
+	public DigitalVideoDisc(String title) {
+		this.title = title;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
+	
+	public void printDetail() {
+		System.out.println("title: "+getTitle());
+		System.out.println("Category: "+getCategory());
+		System.out.println("Director: "+getDirector());
+		System.out.println("Length: "+getLength());
+		System.out.println("Cost: "+getCost());
+	}
+}
